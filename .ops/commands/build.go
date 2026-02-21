579dda1564ed7463c6ab5568726b03d2e3f52891
kiloexabyte <vothanhdat278+1@gmail.com>
2026-02-21 17:06:54 -0600

    fix: use Ubuntu 22.04 with webkit2gtk-4.0

--- .ops/commands/build.go
+++ .ops/commands/build.go
@@ -12,8 +12,7 @@ func (Ops) Build() {
 	ctx := context.Background()
 	sh := command.Shell(sys.Machine(), "wails")
 
-	args := []string{"wails", "build", "-tags", "webkit2gtk_4.1"}
-	if err := sh.Exec(ctx, args...); err != nil {
+	if err := sh.Exec(ctx, "wails", "build"); err != nil {
 		log.Fatal(err)
 	}
 }
